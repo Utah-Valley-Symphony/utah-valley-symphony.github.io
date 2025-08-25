19d28f3308827873e090466b88d5cd3f4016f5e7
Mikkel Davis <mikkelrd@users.noreply.github.com>
2025-08-24 20:21:13 -0600

    Fix quotes

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,14 @@
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("images");
+	eleventyConfig.addPassthroughCopy('images');
+	eleventyConfig.addPassthroughCopy('pages/**/*.css', {
+		mode: 'html-relative',
+	});
 
 	return {
 		dir: {
-			input: "pages",
-			includes: "../includes",
-			output: "_site",
+			input: 'pages',
+			includes: '../includes',
+			output: '_site',
 		},
 	};
 };
